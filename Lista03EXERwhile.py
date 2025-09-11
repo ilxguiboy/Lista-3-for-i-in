1aee31c83239b001f0e8317037217c7cbc48fab9
ilxguiboy <ilxguiboy2029@outlook.com>
2025-09-11 09:32:53 -0300

    Add files via upload

---
+++ Lista03EXERwhile.py
@@ -0,0 +1,38 @@
+# Exercício 1
+# contador = 0
+# soma_positivos = 0
+# quant_negativos = 0
+
+# while contador < 20:
+#     valor = int(input(f"Digite o {contador+1}º valor inteiro: "))
+#     if valor > 0:
+#         soma_positivos += valor
+#     elif valor < 0:
+#         quant_negativos += 1
+#     contador += 1
+
+# print("Soma dos números positivos:", soma_positivos)
+# print("Quantidade de valores negativos:", quant_negativos)
+
+# Exercício 2
+# a=100
+# while a<=200:
+#     if a%2!=0:
+#         print(a)
+#     a=a+1   
+
+# Exercício 3
+# N = int(input("Digite um número inteiro N: "))
+# soma = 0
+# contador = 1
+# while contador <= N:
+#     soma = soma + contador
+#     contador = contador + 1
+# print("A soma de todos os números inteiros de 1 a", N, "é:", soma) 
+    
+# Exercício 5     
+# num = 2
+# while num <= 50:
+#     print(num)
+#     num += 2 
+
